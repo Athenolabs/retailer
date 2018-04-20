d446e701976675cd69901d06e31dde96a4724093
Jigar Tarpara <jigartarpara68@gmail.com>
2018-04-20 15:05:00 +0530

    Print Format Added into Fixtures List

--- retailer/hooks.py
+++ retailer/hooks.py
@@ -17,7 +17,7 @@ app_license = "MIT"
 # include js, css files in header of desk.html
 # app_include_css = "/assets/retailer/css/retailer.css"
 # app_include_js = "/assets/retailer/js/retailer.js"
-fixtures = ["Custom Field"]
+fixtures = ["Custom Field","Print Format"]
 # include js, css files in header of web template
 # web_include_css = "/assets/retailer/css/retailer.css"
 # web_include_js = "/assets/retailer/js/retailer.js"
